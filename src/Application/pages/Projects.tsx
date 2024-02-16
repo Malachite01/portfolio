4fc1c1e5cc3d9384b3d959608ec80b7fa2e3c991
Mathieu Antunes <mathieu.antunes31@gmail.com>
2024-02-16 15:00:08 +0100

    Commit

--- src/Application/pages/Projects.tsx
+++ src/Application/pages/Projects.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import placeHolder from "../../img/projects/placeholder.png";
+import electronApp from "../../img/projects/electron-app.png";
+import deezerLogo from "../../img/projects/deezer-logo.jpg";
+import polyrythm from "../../img/projects/polyrythm.png";
+import game from "../../img/projects/jeu.png";
+import noise from "../../img/projects/noise.png";
+import tri from "../../img/projects/tri-21.png";
 
 const Projects = () => {
   function PrevArrow(props: any) {
@@ -27,17 +32,26 @@ const Projects = () => {
     centerMode: true,
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    autoPlay: true,
+    autoPlaySpeed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
       {
         breakpoint: 580,
         settings: {
-         fade: true,
-         centerMode: false,
-        }
-      },
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+        },
+      }
     ],
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />
@@ -48,20 +62,27 @@ const Projects = () => {
   };
 
   const projectList = [
-    { imageSrc: placeHolder, title: 'Projet 1', description: 'Un projet de ouf' },
-    { imageSrc: placeHolder, title: 'Projet 2', description: 'Un autre projet cool' },
+    { gitUrl: "https://github.com/Malachite01/appTransfertMobile", imageSrc: electronApp, title: '📲 Application electron/nodeJS', description: "Conçue pour transférer automatiquement toutes les photos et fichiers d'un téléphone Android vers un ordinateur en un clic. Son objectif est de simplifier le processus de création de sauvegardes locales régulières." },
+    { gitUrl: "https://github.com/Malachite01/deezer_to_mp3", imageSrc: deezerLogo, title: '🎵 Deezer to MP3', description: 'Un Script Python permettant de sauvegarder localement votre playlist Deezer.' },
+    { gitUrl: "https://github.com/Malachite01/polyrhythm_visualizer", imageSrc: polyrythm, title: '🎼 Visualiseur polyrythmique', description: "Un outil affichant simultanément plusieurs motifs rythmiques complexes pour visualiser les relations entre sons et image. Développé avec une approche mathématique à l'esprit, avec Unity C#." },
+    { gitUrl: "https://github.com/Malachite01/Trisomie21-HG", imageSrc: tri, title: '🪙 Application Trisomie 21', description: "Une application web d'économie de jetons créée pour l'association 'Trisomie 21 Haute-Garonne' afin d'aider les enfants autistes et trisomiques." },
+    { gitUrl: "https://github.com/Malachite01/Image_processing_project", imageSrc: noise, title: "🖼️ Traitement d'images", description: "Un projet de traitement d'images permettant de réduire le bruit ou d'ajouter du bruit à des images en niveaux de gris en utilisant différents types de traitements mathématiques." },
+    { gitUrl: "https://github.com/Malachite01", imageSrc: game, title: '🎮 Jeu video', description: "Un jeu vidéo en cours de développement en autodidacte, développé avec Unity C#." },
   ];
 
   return (
     <div id="projects" className="component projects" data-testid='projects-component'>
-      <div className="slider-container">
+      <h1 className="title">Projets</h1>
+      <div className="slider-container" transition-style="in:wipe:top-right">
         <Slider {...settings}>
           {projectList.map((project, index) => (
             <div className="project-container" key={index}>
               <div className="project">
-                <h1>{project.title}</h1>
                 <img src={project.imageSrc} alt={getImageFileName(project.imageSrc)} />
-                <p>{project.description}</p>
+                <div className="project-text">
+                  <h1><a href={project.gitUrl} target="_blank">{project.title}</a></h1>
+                  <p>{project.description}</p>
+                </div>
               </div>
             </div>
           ))}
