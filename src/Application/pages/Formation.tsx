cdb12fb60ba6891204bb28a6a3f8a97815abd2ab
Mathieu Antunes <mathieu.antunes31@gmail.com>
2025-07-26 00:50:26 +0200

    MAJ dimensions portrait

--- src/Application/pages/Formation.tsx
+++ src/Application/pages/Formation.tsx
@@ -7,7 +7,7 @@ import dashed from "../../img/dashed.svg";
 
 const Formation = () => {
   const formationsList = [
-    { url: "https://www.insa-toulouse.fr/formation/ingenieur-specialite-informatique-et-reseaux/", imageSrc: insa, annee: "INSA Toulouse - 2027", diplome: "Ingénieur Informatique et Réseaux", description : "Préorientation 3° année 'Modélisation, Informatique et Communication'", mention: "En cours d'obtention"},
+    { url: "https://www.insa-toulouse.fr/formation/ingenieur-specialite-informatique-et-reseaux/", imageSrc: insa, annee: "INSA Toulouse - 2028", diplome: "Ingénieur Informatique et Réseaux", description : "Préorientation 3° année 'Modélisation, Informatique et Communication'", mention: "En cours d'obtention"},
     { url: "https://iut.univ-tlse3.fr/bachelor-specialite-informatique", imageSrc: iut, annee: "IUT Informatique Paul Sabatier - 2024", diplome: "Bachelor Universitaire de Technologie (BUT)", description : "Spécialité développement d'applications communicantes et sécurisées", mention: "Mention Bien"},
     { url: "https://www.insee.fr/fr/metadonnees/definition/c2151", imageSrc: iut, annee: "IUT Informatique Paul Sabatier - 2023", diplome: "Diplôme Universitaire de Technologie (DUT)", description : "Spécialité développement d'applications communicantes et sécurisées", mention: "Mention 'Bien'"},
     { url: "#", imageSrc: lycee, annee: "Lycée Général Vincent Auriol - 2021", diplome: "Baccalauréat Général", description : "Spécialités Mathématiques et Physique-Chimie, options Mathématiques expertes et Classe européenne.", mention: "Mention 'Bien'"},
